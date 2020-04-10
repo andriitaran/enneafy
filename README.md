68ccbee3ad60ba1d26575640227bb8f6622388fc
Andrii Taran <andrewtarans@gmail .com>
2020-04-10 13:56:53 -0700

    minor stylying fixes, readme updated

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"Enneafy" is an application that plays music based on your Enneagram type.
+# ENNEAFY
+
+A mobile app that will help you to find out your enneagram type and play curated music based on it!
+
+<img width="1280" alt="perfect_cup" src="https://i.ibb.co/k4znx4m/enneafy.png">
+
+## PROBLEM
+
+This app was made during 1-Day Sprint during my diploma program at BrainStation. We were tasked to make a website/web application using technologies we've learned so far - HTMl, CSS(Sass), Javascript, React, Node, Express and Web API's.
+
+## APP VIEWPORT :
+
+Currently the app is setup for iPhone 8+ viewport(414x736 px), but could be easily adapted to other screen sizes. It will be always centered if you use a bigger viewport size.
+
+## TECH STACK
+
+HTML, CSS, Javascript, React, Node, Express
+
+Public API: Spotify | https://developer.spotify.com/documentation/web-api/
--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,33 +4,27 @@ import Logo from "../assets/icons/enneagram.svg";
 
 export default function Welcome() {
   return (
-    <div>
-      <div>
-        <section className="welcome">
-          <div className="welcome-container">
-            <span className="welcome-container__header">Enneafy</span>
-            <img className="welcome-container__logo" src={Logo} alt="logo" />
-            <span className="welcome-container__text">
-              Enneagram is one of the most powerful and insightful tools for
-              understanding ourselves and others. At its core, the Enneagram
-              helps us to see ourselves at a deeper, more objective level and
-              can be of invaluable assistance on our path to self-knowledge.
-            </span>
-            <a
-              href="https://www.truity.com/test/enneagram-personality-test"
-              target="_blank"
-            >
-              <button className="welcome-container__test">TAKE THE TEST</button>
-            </a>
+    <section className="welcome">
+      <div className="welcome-container">
+        <span className="welcome-container__header">Enneafy</span>
+        <img className="welcome-container__logo" src={Logo} alt="logo" />
+        <span className="welcome-container__text">
+          Enneagram is one of the most powerful and insightful tools for
+          understanding ourselves and others. At its core, the Enneagram helps
+          us to see ourselves at a deeper, more objective level and can be of
+          invaluable assistance on our path to self-knowledge.
+        </span>
+        <a
+          href="https://www.truity.com/test/enneagram-personality-test"
+          target="_blank"
+        >
+          <button className="welcome-container__test">TAKE THE TEST</button>
+        </a>
 
-            <Link to="/types">
-              <button className="welcome-container__type">
-                I KNOW MY TYPE!
-              </button>
-            </Link>
-          </div>
-        </section>
+        <Link to="/types">
+          <button className="welcome-container__type">I KNOW MY TYPE!</button>
+        </Link>
       </div>
-    </div>
+    </section>
   );
 }
